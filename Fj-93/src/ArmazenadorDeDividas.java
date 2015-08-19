c1d9e2bd05090a7c069685a8bf8d4a71553b05b0
Renato <Renato@Natho>
2015-08-18 23:45:39 -0300

    Armazenador de Dividas Interface

--- Fj-93/src/ArmazenadorDeDividas.java
+++ Fj-93/src/ArmazenadorDeDividas.java
@@ -1,4 +1,8 @@
-
+/**
+ * Interface ArmazenadorDeDividas
+ * @author Renato
+ *
+ */
 public interface ArmazenadorDeDividas {
 	void salva(Divida divida);
 	Divida carrega(Documento documentoCredor);
